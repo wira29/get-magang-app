9a2ce7f1d59ddbee8ee5371b384bce49660931a0
Wira29 <bag4sk4r4@gmail.com>
2022-11-02 14:04:20 +0700

    layouting home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,7 +12,39 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('RFID Scanner'),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/illustrations/rfid_illustration.png',
+                width: 200,
+                height: 200,
+              ),
+              const Text(
+                "Silakan melakukan scan id card anda pada RFID scanner !",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Scan ID Card Anda',
+                  ),
+                  autofocus: true,
+                  onFieldSubmitted: (value) {
+                    print(value);
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
